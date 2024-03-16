5842fad2cbf6d9ed55e015c03215085fe73bb67f
Zhiwei Li <zhiwei.li2@mail.mcgill.ca>
2024-03-15 22:41:21 -0700

    add router

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,6 @@
 import { ReactElement } from 'react';
 import styled from 'styled-components';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import { ActivateDeactivate } from './components/ActivateDeactivate';
 import { Greeter } from './components/Greeter';
 import { SectionDivider } from './components/SectionDivider';
@@ -10,17 +11,35 @@ const StyledAppDiv = styled.div`
   display: grid;
   grid-gap: 20px;
 `;
-
 export function App(): ReactElement {
   return (
-    <StyledAppDiv>
-      <ActivateDeactivate />
-      <SectionDivider />
-      <WalletStatus />
-      <SectionDivider />
-      <SignMessage />
-      <SectionDivider />
-      <Greeter />
-    </StyledAppDiv>
+    <Router>
+      <StyledAppDiv>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">ActivateDeactivate</Link>
+            </li>
+            <li>
+              <Link to="/wallet-status">WalletStatus</Link>
+            </li>
+            <li>
+              <Link to="/sign-message">SignMessage</Link>
+            </li>
+            <li>
+              <Link to="/greeter">Greeter</Link>
+            </li>
+          </ul>
+        </nav>
+        <Routes>
+          <Route path="/" element={<ActivateDeactivate />} />
+          <Route path="/wallet-status" element={<WalletStatus />} />
+          <Route path="/sign-message" element={<SignMessage />} />
+          <Route path="/greeter" element={<Greeter />} />
+          {/* If you have a not found page or want to redirect to a default, you can use a Route like this */}
+          {/* <Route path="*" element={<NotFoundComponent />} /> */}
+        </Routes>
+      </StyledAppDiv>
+    </Router>
   );
 }
