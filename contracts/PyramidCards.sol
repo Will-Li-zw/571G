7aea33c10d16f449565765d606ef7abc2ca88585
Zichen Chang <changzichen2000@gmail.com>
2024-03-17 22:00:28 -0700

    Backend Contract with Random implementation

--- contracts/PyramidCards.sol
+++ contracts/PyramidCards.sol
@@ -1,14 +1,17 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-contract PyramidCards {
+import "@chainlink/contracts/src/v0.8/interfaces/VRFCoordinatorV2Interface.sol";
+import "@chainlink/contracts/src/v0.8/vrf/VRFConsumerBaseV2.sol";
+
+contract PyramidCards is VRFConsumerBaseV2 {
 
     struct Card {
         uint256 id;
         address owner;
-        uint256 price;      // # TODO: we may cancel the market setting of the project
-        bool isForSale;
-    }
+        string collection;
+        uint256 amount; 
+        }
 
     // ============================================== StateVariables ==============================================
     uint256 public constant PRICE = 0.001 ether;
@@ -17,12 +20,23 @@ contract PyramidCards {
     mapping(address => uint256) public userBalances;     // balance of each user
     mapping(address => Card[]) public userCollection;    // cards collection of each user
 
-    mapping(string => uint8[]) public collectionCards;   // Collection "A" -> id [1,2,3,4,5]
-    mapping(uint8 => uint8) public collectionProbability;// id -> probability of each id
-    mapping(string => uint256) public collectionAward;   // Collection "A" -> award value
+    string[] public collections;   // Collection "A" -> id [1,2,3,4,5] id are fixed
+    mapping(string => string) public collectionAward;   // Collection "A" -> award value
 
-    Card[] public cardForSale;                           // cards for sale  # TODO: we may cancel the market setting of the project
+    // Card[] public cardForSale;                           // cards for sale  # TODO: we may cancel the market setting of the project
 
+    VRFCoordinatorV2Interface private immutable i_vrfCoordinator;
+    // Variables for chainlink random number function:
+    bytes32 private immutable i_gasLane;
+    uint64 private immutable i_subscriptionId;
+    uint32 private immutable i_callBackGasLimit;
+    uint32 private constant NOM_WORDS = 1;
+    uint16 private constant REQEUST_CONFIRMATIONS = 3;
+
+    uint256 private constant MAX_CHANCE_VALUE = 100;
+    // record mapping
+    mapping(uint256 => address) s_requestIdToSender;
+    mapping(uint256 => string) s_requestIdToCollection;
 
     // ============================================== Modifiers ==============================================
     modifier isAdmin(){
@@ -31,7 +45,7 @@ contract PyramidCards {
     }
 
     modifier drawPriceEnough(){
-        require(msg.value == PRICE, "Ether not enough, cannot draw");
+        require(userBalances[msg.sender] > 0, "Drawchance not enough, cannot draw");
         _;
     }
 
@@ -43,8 +57,18 @@ contract PyramidCards {
 
     // ============================================== User Functions ==============================================
     // constructors
-    constructor() {
+    constructor(
+        address vrfCoordinatorV2,
+        bytes32 gasLane,
+        uint64 subscriptionId,
+        uint32 callBackGasLimit
+    ) VRFConsumerBaseV2(vrfCoordinatorV2) {
         admin = msg.sender;
+
+        i_vrfCoordinator = VRFCoordinatorV2Interface(vrfCoordinatorV2);
+        i_gasLane = gasLane;
+        i_subscriptionId = subscriptionId;
+        i_callBackGasLimit = callBackGasLimit;
     }
 
     // User can use this to add balance to theirselves
@@ -63,44 +87,67 @@ contract PyramidCards {
         require(ok, "Failed to withdraw ether");
     }
 
-    // User can draw card
-    function drawCard() external payable drawPriceEnough {
+    // User can draw card given he/she has enough ether
+    function drawRandomCard(string memory collection) external drawPriceEnough {
         // Random function of chainlink:
-        uint256 randomId = 0;
-        // Retrieve the random card, modify the owner information
-        Card memory card = Card({
-            id: randomId,
-            owner: msg.sender,
-            price: 0,
-            isForSale: false
-        });
-        // Add to the user's collection
-        userCollection[msg.sender].push(Card({
-            id: randomId,
-            owner: msg.sender,
+        uint256 requestId = i_vrfCoordinator.requestRandomWords(
+            i_gasLane,
+            i_subscriptionId,
+            REQEUST_CONFIRMATIONS,
+            i_callBackGasLimit,
+            NOM_WORDS
+        );
+
+        s_requestIdToSender[requestId] = msg.sender;    // record requestId to address
+        s_requestIdToCollection[requestId] = collection;    // record requestId to collection we want to draw
+    }
+
+    function fulfillRandomWords(uint256 requestId, uint256[] memory randomWords) internal override {
+        address cardOwner = s_requestIdToSender[requestId];
+        uint256 rng = randomWords[0] % MAX_CHANCE_VALUE;
+        uint256 cardId = getCardIdFromRng(rng); // cardId from 1 to 5
+        string memory collection = s_requestIdToCollection[requestId];
+
+        // user draw this card!
+        userCollection[cardOwner].push(Card({
+            id: cardId,
+            owner: cardOwner,
+            collection: collection,
             price: 0,
             isForSale: false
         }));
     }
 
-    // make cards for sale
-    function addMyCardForSale(uint256 id) public {
-
+    // chance array
+    function getChanceArray() public pure returns(uint256[5] memory) {
+        // 0 - 24, 25 - 49, 50 - 74: 1,2,3
+        // 75 - 89: 4
+        // 90 - 99: 5
+        return [25, 50, 75, 90, MAX_CHANCE_VALUE];
     }
 
-    // buy the card with id
-    function buyCard(uint256 id) public {
-
+    // get cardId from the chance array and rng
+    function getCardIdFromRng(uint256 rng) private pure returns(uint256) {
+        uint256 cumulativeSum = 0;
+        uint256[5] memory chanceArray = getChanceArray();
+        uint256 i;
+        for(i = 0; i < chanceArray.length; i++) {
+            if(rng >= cumulativeSum && rng < cumulativeSum + chanceArray[i]) {
+                break;
+            }
+            cumulativeSum = cumulativeSum + chanceArray[i];
+        }
+        return i+1;
     }
 
     // User want to exchange two cards for one draw chance
-    function exchangeForDraw(uint256 id_1, uint256 id_2) public {
-
+    function exchangeForDraw(string memory collection, uint256 id) public {
+        
     }
 
     // ============================================== Admin Functions ==============================================
     // create a new collection of cards
-    function createCollectionofCards(string memory name, uint256 id) external isAdmin {
+    function createCollections(string memory name) external isAdmin {
         // create new card
         // TODO: how to deal with images
 
@@ -108,12 +155,8 @@ contract PyramidCards {
     }
 
     // create a new collection of cards
-    function setCollectionAward(string memory name, uint256 awardValue) external isAdmin {
+    function setCollectionAward(string memory name, string memory awardValue) external isAdmin {
         collectionAward[name] = awardValue;
     }
 
-    // get all cards that for sale
-    function getAllForSale() public view returns (Card[] memory) {
-        return cardForSale;
-    }
 }
